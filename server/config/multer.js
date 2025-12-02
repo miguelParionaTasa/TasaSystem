f145998045afca9de8cc9b51d465a57148fef685
Miguel Angel Pariona Nuñez <mpariona@tasa.com.pe>
2025-12-01 23:14:43 -0500

    fix

--- server/config/multer.js
+++ server/config/multer.js
@@ -1,12 +1,12 @@
 // config/multer.js
 const multer = require("multer");
 
-// Usar almacenamiento en memoria (en lugar de en disco)
-const storage = multer.memoryStorage(); // Guarda los archivos en memoria
+// Guardar archivos en memoria
+const storage = multer.memoryStorage();
 
 const upload = multer({
   storage,
-  limits: { fileSize: 2 * 1024 * 1024 }, // ⛔ máximo 2 MB
-}).single("image"); // "image" es el campo del formulario
+  limits: { fileSize: 8 * 1024 * 1024 }, // ✅ máximo 6 MB
+}).single("image");
 
 module.exports = upload;
